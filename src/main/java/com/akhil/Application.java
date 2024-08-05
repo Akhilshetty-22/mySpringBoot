39bdc4e4ca8c224abbf4af9ee36bd6cafa293e0a
Akhil <settiakhil9@gmail.com>
2024-08-05 02:10:21 -0500

    Enabled Swagger api

--- src/main/java/com/akhil/Application.java
+++ src/main/java/com/akhil/Application.java
@@ -2,6 +2,7 @@ package com.akhil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
--- src/main/java/com/akhil/config/SwaggerConfig.java
+++ src/main/java/com/akhil/config/SwaggerConfig.java
@@ -1,24 +1,53 @@
-package com.akhil.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
-@Configuration
-//@EnableSwagger2
-public class SwaggerConfig {
-    @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.akhil.Controller"))
-                .paths(PathSelectors.any())
-                .build();
-
-    }
-}
+//package com.akhil.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+////import springfox.documentation.oas.annotations.EnableOpenApi;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//
+//@Configuration
+////@EnableSwagger2
+//@EnableWebMvc
+//public class SwaggerConfig implements WebMvcConfigurer {
+//    @Bean
+//    public Docket api(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.akhil.Controller"))
+//                .paths(PathSelectors.regex("/.*"))
+//                .build().apiInfo(apiInfoMetaData());
+//
+//    }
+//
+//    private ApiInfo apiInfoMetaData() {
+//
+//        return new ApiInfoBuilder().title("SALES SERVICE")
+//                .description("API Endpoint Decoration")
+//                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
+//                .license("Apache 2.0")
+//                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+//                .version("1.0.0")
+//                .build();
+//    }
+//
+////    @Override
+////    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+////        registry.addResourceHandler("swagger-ui.html")
+////                .addResourceLocations("classpath:/META-INF/resources/");
+////
+////        registry.addResourceHandler("/webjars/**")
+////                .addResourceLocations("classpath:/META_INF/resources/webjars/");
+////    }
+//
+//}
